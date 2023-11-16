ed668a15c458617887410503ffef4b11d74275e4
Antonio <antoniotrid@gmail.com>
2023-11-16 11:49:11 +0100

    Vector Example

---
+++ Vector.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    vector<int> v;
+    const int n = 10;
+
+    for (int i = 1; i <= n; ++i) {
+        v.push_back(i);
+    }
+
+    for (int i = n - 1; i >= 0; --i) {
+        cout << v[i] << " ";
+    }
+
+    cout << v.size();
+    return(0);
+    
+} 
