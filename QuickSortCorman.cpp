259e5266204155dc46e36c0f6ca34fcfe0145bf5
AntonioTri <antoniotrid@gmail.com>
2023-11-22 23:28:38 +0100

    QuickSort

--- QuickSortCorman.cpp
+++ QuickSortCorman.cpp
@@ -13,16 +13,26 @@ class QuickSort {
 
             if(q - r <= 0) {return;};
 
-            int pivot = Partitioning(a, r, q);
+            int pivot = PartitioningCoreman(a, r, q);
             CoremanQuickSort(a, r, pivot - 1);
             CoremanQuickSort(a, pivot + 1, q); 
+
+        };
+
+        void HoareQuickSort(int *a, int r, int q){
+
+            if (q- r < 0){ return;};
+
+            int pivot = PartitioningHoare(a, r, q);
+            HoareQuickSort(a, r, pivot -1);
+            HoareQuickSort(a, pivot + 1, q);
 
         };
 
     private:
 
 
-        int Partitioning(int *a, int r, int q){
+        int PartitioningCoreman(int *a, int r, int q){
 
             int pivot = a[q];
             int i = r - 1;
@@ -39,6 +49,39 @@ class QuickSort {
 
         };
 
+        int PartitioningHoare(int *a, int r, int q){
+
+            int pivot = a [q];
+            int i = r - 1;
+            int j = q;
+
+            while (true)
+            {
+                while (a[++i] <= pivot && i < q )
+                {
+                    i++;
+                }
+
+                while (a[--j] >= pivot)
+                {
+                    j--;
+                    if (j == r){ break; }
+
+                }
+                
+                if (i < j){
+
+                    Swap(a[i], a[j]);
+
+                } else {
+                    break;
+                }
+            }
+           // Swap(a[i], a[j]);
+            return i;
+        };
+
+
         void Swap(int &a, int &b){
 
             int temp = a;
@@ -54,26 +97,44 @@ class QuickSort {
 
 int main(){
 
+    QuickSort *quickSort = new QuickSort(); 
+
+    //Coreman QuickSort
     int a[LENGHT] = { 2, 53 ,34 ,7 ,5 ,4 ,32 ,23, 25, 53, 46, 54, 26, 23, 13, 56, 7, 77,43};
 
+    cout << "Risoluzione con Partizionamento Coreman" << endl;
     for (int i = 0; i < LENGHT ; i++)
     {
         cout << a[i] << " ";
     }
-    
     cout << endl;
-
-    QuickSort *quickSort = new QuickSort(); 
 
     quickSort->CoremanQuickSort(a, 0, LENGHT - 1);
 
     for (int i = 0; i < LENGHT ; i++)
     {
         cout << a[i] << " ";
+    }    
+    cout << endl << endl;
+
+
+    //Hoare QuickSort
+    int b[LENGHT] = { 2, 53 ,34 ,7 ,5 ,4 ,32 ,23, 25, 53, 46, 54, 26, 23, 13, 56, 7, 77,43};
+
+    cout << "Risoluzione con Partizionamento Hoare" << endl;
+    for (int i = 0; i < LENGHT ; i++)
+    {
+        cout << b[i] << " ";
     }
-    
     cout << endl;
 
+    quickSort->HoareQuickSort(b, 0, LENGHT - 1);
+
+    for (int i = 0; i < LENGHT ; i++)
+    {
+        cout << b[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 
